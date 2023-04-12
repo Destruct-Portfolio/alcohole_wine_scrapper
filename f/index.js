3b1613126e0c7f10cb0f4dfc8eb83682f9877921
Ben Hichem <77390862+benhichem@users.noreply.github.com>
2023-04-12 19:39:30 +0000

    ...

--- f/index.js
+++
@@ -1,2 +0,0 @@
-import Handler from "./components/cors/handler.js";
-console.log(await Handler.exec());
--- f/misc/sleep.js
+++
@@ -1,7 +0,0 @@
-export default class Sleep {
-    static async For(seconds) {
-        return new Promise((resolve) => {
-            setTimeout(resolve, seconds * 1000);
-        });
-    }
-}
